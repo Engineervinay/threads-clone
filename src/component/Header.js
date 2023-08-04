d5a984df59e26c8543b77c361bc9f2aade649dbc
Vinay Patil <vinay1645.vp@gmail.com>
2023-08-04 17:50:56 +0530

    inserted header data

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,22 +1,22 @@
 import React from 'react'
 
-function Header() {
+function Header({user}) {
   return (
     <header>
       <div className='info-container'>
         <div className='user-info-container'>
-            <h1>username</h1>
-            <p>handle <span className='threads-info'>threads.net</span></p>
+            <h1>{user.username}</h1>
+            <p>{user.handle} <span className='threads-info'>threads.net</span></p>
         </div>
         <div className='img-container'>
-            <img src="" alt ="profile avatar"/>
+            <img src={user.img} alt ="profile avatar"/>
 
         </div>
       </div>
       
-        <p>bio</p>
+        <p>{user.bio}</p>
         <div className='sub-info-container'>
-          <p className='sub-text'>x followers - <a href="#">link</a></p>
+          <p className='sub-text'>{user.followers.length} Followers • <a href={user.link}>{user.link.replace("https://","")}</a></p>
         </div>
       <button className='primary' onClick={()=>navigator.clipboard.writeText('profile url')}>Share Profile</button>
       <div className='button-container'>
