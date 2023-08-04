fabed09a44974c56464471c2d7a634429f164b7e
Vinay Patil <vinay1645.vp@gmail.com>
2023-08-04 17:15:41 +0530

    user fetching with json server

--- src/App.js
+++ src/App.js
@@ -6,10 +6,25 @@ import PopUp from './component/PopUp'
 import './App.css';
 
 function App() {
-  
+  const[user,setUser]=useState(null)
+  const userId="e626d981-4318-4188-a640-09dbd13e3241";
+  const getUser=async()=>{
+    try{
+      const response=await fetch(`http://localhost:3000/users?user_uuid=${userId}`);
+      const data=await response.json()
+      setUser(data[0])
+    }catch(error){
+      console.error(error)
+    }
+  }
+
+  useEffect(()=>{
+    getUser()
+  },[])
+  console.log(user)
   return (
     <div className="App">
-      <Nav/>
+      <Nav />
       <Header/>
       <Feed/> 
      {/* <PopUp/>*/}
